a401ce34c37c632d1a4a06d11640896f34fd705b
Sandrea-MichD <sandream456@gmail.com>
2024-12-13 12:05:16 +0530

    Java file for the comment section

--- src/gihan/submitCommentServlet.java
+++ src/gihan/submitCommentServlet.java
@@ -6,44 +6,63 @@ package gihan;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author sandr
- */
-@WebServlet(name = "submitCommentServlet", urlPatterns = {"/submitCommentServlet"})
+
 public class submitCommentServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet submitCommentServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet submitCommentServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        String userIdPara = request.getParameter("user_id");
+        
+        
+            // Check if the user_id parameter is null or empty
+    if (userIdPara.equals("null")) {
+        response.sendRedirect("user_login.jsp");
     }
+    else{
+        
+        
+        
+        try {
+               int user_id = Integer.parseInt(userIdPara);
+               String comment = request.getParameter("comment"); 
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test1login", "root", "");
+                String query = "INSERT INTO comments(user_id,comment) VALUES(?,?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setInt(1, user_id); // Set new status
+                ps.setString(2, comment); // Set booking ID
+
+                int rowsUpdated = ps.executeUpdate();
+                if(rowsUpdated>0){
+                out.println("<script>");
+                out.println("alert('Comment Added successfully!');");
+                out.println("window.location.href = 'booking_dashboard.jsp';"); 
+                out.println("</script>");
+                }
+        }
+            catch(Exception e){
+                out.println(e);
+            }
+    }
+        
+        
+        
+       
+ }
+}
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
